77f05a660f7002d91c226e6bdd3ba0d95d19ee7e
Artem Kovera <artemkovera@gmail.com>
2020-06-05 19:34:48 +0300

    reimplemented stack the data structure

--- stack.cpp
+++ stack.cpp
@@ -3,15 +3,16 @@
 #include<iostream>
 #include<stdexcept>
 
+
+
 class Stack
 {
-
 public:
     //default constructor creates an empty stack with default size, which is 100
     Stack ();
 
     //this constructor creates an empty stack whose size is equal to the parameter of this costructor
-    explicit Stack (const int);
+    explicit Stack (size_t);
 
     Stack (const Stack&) = delete;
 
@@ -24,9 +25,11 @@ public:
     virtual ~Stack ();
 
     //method to add element onto the stack
-    void push (const int);
+    //if the stack is full, the method does nothing
+    void push (int);
 
     //method to delete a top element from the stack
+    //if the stack is empty, the method does nothing   
     void pop ();
 
     //method to get the value of the first element in the stack 
@@ -40,102 +43,94 @@ public:
     bool isEmpty () const;
 
     //this method returns operational size of the stack
-    int getSize () const;
+    size_t getSize () const;
 
 private:
-    int size;
+    //default size of the stack created by default constructor
+    static const size_t defaultSize = 100;
 
-    int* St = nullptr;
+    //size of the stack
+    size_t size;    
 
-    int* topPointer = nullptr; 
+    //pointer pointing to the start of the stack
+    int* startPointer = nullptr;
 
-    //this flag indicates whether the default constructor was called
-    bool flagDefConstruct;      
+    //pointer pointing to the top of the stack
+    int* topPointer = nullptr;  
 
     //helper method for freeing up memory
-    void clean ();
+    void clean () noexcept;
 
     //helper method used in move constructor and assignment operator
     void moveFrom (Stack&) noexcept;
 };
 
 int main ()
-{
-    Stack s1(4);
+{ 
+    Stack st1(2);
 
-    if (s1.isEmpty())
-        std::cout << "now s1 is empty" << std::endl; 
+    if(st1.isEmpty())
+    {
+        std::cout<<"The stack is empty"<<std::endl;
+    }
 
-    s1.push(1);
+    if(st1.isFull())
+    {
+        std::cout<<"The stack is full"<<std::endl;
+    }
 
-    if (s1.isFull())
-        std::cout << "s1 is full" << std::endl;         
+    st1.push(10);
 
-    s1.push(2);
-    s1.push(3);
-    s1.push(4);
-     
-    if (s1.isFull())
-        std::cout << "now s1 is full" << std::endl;    
+    std::cout<<st1.top()<<std::endl;
+
+    if(st1.isFull())
+    {
+        std::cout<<"The stack is full"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"The stack is NOT full"<<std::endl;
+    }
+
+    st1.pop();
     
-    std::cout << "s1 top is " << s1.top() << std::endl;
+    if(st1.isEmpty())
+    {
+        std::cout<<"The stack is empty"<<std::endl;
+    }
+    std::cout<<st1.getSize()<<std::endl;
 
-    s1.pop();
+    Stack st2;
 
-    if (s1.isFull())
-        std::cout << "now s1 is full" << std::endl; 
-    else    
-        std::cout << "after poping an element, s1 is NOT full" << std::endl;
+    std::cout<<"st2 has " << st2.getSize() << " elements" << std::endl;
+    if(st2.isEmpty())
+    {
+        std::cout<<"The st2 stack is empty"<<std::endl;
+    }
 
-    std::cout << "now s1 top is " << s1.top() << std::endl;
-    std::cout << "The operational size of s1 is " << s1.getSize() << std::endl;
+    st2.push(25);
+    if(st2.isEmpty())
+    {
+        std::cout<<"The st2 stack is empty"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"The st2 stack is not empty"<<std::endl;
+    }
 
-    ////
-    std::cout << std::endl;
-    Stack s2(std::move(s1));
-    if (s2.isFull())
-        std::cout << "s2 is full" << std::endl; 
-    else    
-        std::cout << "s2 is NOT full" << std::endl;
+    st2 = std::move(st1);
 
-    std::cout << "s2 top is " << s2.top() << std::endl;
+    std::cout<<"Now st2 has " << st2.getSize() << " elements" << std::endl;
 
-    ////
-    std::cout << std::endl;
-    Stack s3(2);
-    s3.push(33);
-    s3.push(32);
-    if (s3.isFull())
-        std::cout << "s3 is full" << std::endl; 
-    else    
-        std::cout << "s3 is NOT full" << std::endl;
-
-    std::cout << "s3 top is " << s3.top() << std::endl;  
-    
-    s3 = std::move(s2);  
-    if (s3.isFull())
-        std::cout << "after assigning to s2, s3 is full" << std::endl; 
-    else    
-        std::cout << "after assigning to s2, s3 is NOT full" << std::endl;
-
-    std::cout << "after assigning to s2, s3 top is " << s3.top() << std::endl;      
-
-    ////
-    std::cout << std::endl;
-    Stack s4;
-    if (s4.isEmpty())
-        std::cout << "now s4 is empty" << std::endl; 
-    else    
-        std::cout << "now s4 is NOT empty" << std::endl;
-    std::cout << "The operational size of s4 is " << s4.getSize() << std::endl;
-    
-    
     return 0;
 }
 
-Stack::Stack(): size{101}, St{new int [size + 1]}, topPointer{St}, flagDefConstruct{true} {}
 
-Stack::Stack(const int val): size{val}, St{new int [size]}, topPointer{St}, flagDefConstruct{false} {}
+Stack::Stack(): size{defaultSize}, startPointer{new int [size + 1]}, topPointer{startPointer}  
+{}
+
+Stack::Stack(size_t val): size{val}, startPointer{new int [val + 1]}, topPointer{startPointer}
+{}
 
 Stack::Stack(Stack&& src) noexcept
 {
@@ -144,11 +139,11 @@ Stack::Stack(Stack&& src) noexcept
 
 Stack& Stack::operator=(Stack&& src) noexcept
 {
-    if (this == &src) return *this;
-
+    if(this == &src)
+        return *this;
+    
     clean();
     moveFrom(src);
-
     return *this;
 }
 
@@ -157,66 +152,68 @@ Stack::~Stack()
     clean();
 }
 
-bool Stack::isEmpty() const
+void Stack::push(int val)
 {
-    if(topPointer == St) return true;
-    
-    return false;
+    if(isFull())
+        return;
+
+    topPointer++;
+    *topPointer = val;
 }
 
 bool Stack::isFull() const
 {
-    if(topPointer == (St + size)) return true;
-
-    return false; 
+    if(topPointer == startPointer+size)
+        return true;
+    else
+        return false;
 }
 
-void Stack::push(const int val)
+bool Stack::isEmpty() const
 {
-    if(isFull()) return;
+    if(topPointer == startPointer)
+        return true;
+    else
+        return false;
+}
 
-    *(topPointer + 1) = val;
-   
-    topPointer++;
+int Stack::top() const 
+{
+    if(isEmpty()) 
+        throw std::out_of_range("The stack is empty");
+
+    return *topPointer;
 }
 
 void Stack::pop()
 {
-    if(isEmpty()) return;
-
+    if(isEmpty()) 
+        return;
+    
     topPointer--;
 }
 
-int Stack::top() const
+size_t Stack::getSize() const
 {
-    if(isEmpty()) throw std::out_of_range("The stack is empty");
-    
-    return *topPointer;
-}
-
-int Stack::getSize() const
-{
-    if(flagDefConstruct) return 100;
-
     return size;
 }
 
-void Stack::clean()
-{
-    delete [] St;
-    St = nullptr;
+void Stack::clean() noexcept
+{   
+    delete [] startPointer;
+    startPointer = nullptr;
     topPointer = nullptr;
 }
 
 void Stack::moveFrom(Stack& src) noexcept
 {
     size = src.size;
-    St = src.St;
+    startPointer = src.startPointer;
     topPointer = src.topPointer;
 
     src.size = 0;
-    src.St = nullptr;
-    src.topPointer = nullptr;    
+    src.startPointer = nullptr;
+    src.startPointer = nullptr;
 }
 
 
