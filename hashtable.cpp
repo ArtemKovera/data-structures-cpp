18dc0fca314d7115d55ad0633c7ce7c458a6640d
Artem Kovera <artemkovera@gmail.com>
2020-06-08 10:04:23 +0300

    reimplemented the hash table data structure. there are no memory errors detected by valgrind

--- hashtable.cpp
+++ hashtable.cpp
@@ -6,10 +6,13 @@
 class HashTable
 {
 public:
-     
+    //default constructor creates an empty hash table
     HashTable ();
     
+    //this constructor creates a hash table with one entry equaled to the constructor's argument
     HashTable (const int);
+
+    virtual ~HashTable();
 
     HashTable (const HashTable&) = delete;
 
@@ -17,123 +20,141 @@ public:
 
     HashTable& operator=(HashTable&&) = delete;
 
-    HashTable (HashTable&&) = delete;
-
-    virtual ~HashTable ();
+    HashTable (HashTable&&) = delete;       
     
+    //this method returns true if a given value is in a hash table and false otherwise
     bool search (const int) const;
-
+    
+    //this method inserts a value to a hash table
+    //if the value is in the hash table already, the method does nothing
     void insert (const int);
-
+    
+    //this method removes a value from a hash table
     void remove (const int);
-
-    int size () const;
+    
+    //this method returns the total number of entries in a hash table
+    size_t size () const;    
 
 private:
-
     //this is method for a hash function
     //the hash function in this method is simple h(x) = x%10 
     int hashFunction (const int) const;
     
+    //underlying linked list data structure 
     struct LinkedList final
     {
         LinkedList ();
+        ~LinkedList();
         struct Node
         {
             int data;
             Node* next;
         };
-        int nodeCount;
+        size_t nodeCount;
         Node* head;
         Node* buildNode (int);
-        int getNodeCount () const;
-        void sortedInsert (const int);
-        void remove (const int);
-        bool search (const int) const;
-        void clean (Node*);
+        size_t getNodeCount () const;
+        void insertToList (const int);
+        void removeFromList (const int);
+        bool searchInList (const int) const;
+        void cleanList () noexcept;
     }; 
 
     //array of 20 nested Linked List data structures
     LinkedList* arrayOfLinkedLists = new LinkedList [20];
 
-    int totalCountOfEntries;
-
 };
 
-int main ()
+int main()
 {
+    //Testing the general functionality of the hash-table data structure
     HashTable ht1;
+    std::cout<<"the initial size of ht1 is " << ht1.size() << std::endl;
 
-    ht1.insert(45);
-    ht1.insert(77);
-    ht1.insert(-10000); 
-    ht1.insert(0);
-    ht1.insert(-9);
-
-    if (ht1.search(45))
-        std::cout << "45 is in ht1" << std::endl;
+    if(ht1.search(234))
+        std::cout<<"234 is in ht1"<<std::endl;
     else
-        std::cout << "45 is NOT in ht1" << std::endl;
-    
-    if (ht1.search(77))
-        std::cout << "77 is in ht1" << std::endl;
+        std::cout<<"234 is NOT in ht1"<<std::endl;
+    std::cout << std::endl;
+
+    ht1.insert(234);
+    if(ht1.search(234))
+        std::cout<<"after 234 insertion, 234 is in ht1"<<std::endl;
     else
-        std::cout << "77 is NOT in ht1" << std::endl;
+        std::cout<<"234 is NOT in ht1"<<std::endl;    
+    std::cout<<"the size of ht1 after 1 insertion is " << ht1.size() << std::endl;
+    std::cout << std::endl;
 
-    if (ht1.search(-10000))
-        std::cout << "-10000 is in ht1" << std::endl;
+    ht1.insert(-2789);
+    if(ht1.search(-2789))
+        std::cout<<"after -2789 insertion, 234 is in ht1"<<std::endl;
     else
-        std::cout << "-10000 is NOT in ht1" << std::endl;
+        std::cout<<"-2789 is NOT in ht1"<<std::endl;        
+    std::cout<<"the size of ht1 after 2 insertions is " << ht1.size() << std::endl;
+    std::cout << std::endl;
 
-    if (ht1.search(0))
-        std::cout << "0 is in ht1" << std::endl;
+    ht1.insert(4004);
+    if(ht1.search(4004))
+        std::cout<<"after 4004 insertion, 4004 is in ht1"<<std::endl;
     else
-        std::cout << "0 is NOT in ht1" << std::endl; 
+        std::cout<<"4004 is NOT in ht1"<<std::endl;    
+    std::cout<<"the size of ht1 after 3 insertions is " << ht1.size() << std::endl; 
+    std::cout << std::endl;
 
-    if (ht1.search(-9))
-        std::cout << "-9 is in ht1" << std::endl;
+    ht1.remove(4004);
+    if(ht1.search(4004))
+        std::cout<<"after removing 4004 , 4004 is still in ht1"<<std::endl;
     else
-        std::cout << "-9 is NOT in ht1" << std::endl;                            
+        std::cout<<"after removing 4004, 4004 is NOT in ht1"<<std::endl;    
+    std::cout<<"the size of ht1 after 3 insertions and one delition is " << ht1.size() << std::endl; 
+    std::cout<<std::endl;
 
-    if (ht1.search(999))
-        std::cout << "999 is in ht1" << std::endl;
+    HashTable ht2(987777);
+    std::cout<<"the initial size of ht2 is " << ht2.size() << std::endl;
+
+    if(ht2.search(987777))
+        std::cout<<"987777 is in ht2"<<std::endl;
     else
-        std::cout << "999 is NOT in ht1" << std::endl;
+        std::cout<<"987777 is NOT in ht2"<<std::endl;
+    std::cout << std::endl;
 
-    if (ht1.search(-11))
-        std::cout << "-11 is in ht1" << std::endl;
+    ht2.insert(-1000);
+    if(ht2.search(-1000))
+        std::cout<<"after -1000 insertion, -1000 is in ht1"<<std::endl;
     else
-        std::cout << "-11 is NOT in ht1" << std::endl;          
-    
+        std::cout<<"-1000 is NOT in ht1"<<std::endl;    
+    std::cout<<"the size of ht2 after 1 insertion is " << ht2.size() << std::endl;
+    std::cout << std::endl;
 
-    std::cout << "ht1 now contains " << ht1.size() << " elements" << std::endl;  
-
-    std::cout << "-----------------------" << std::endl;
-    HashTable ht2 (577);
-    ht2.insert(-34);
-    ht2.insert(-34);
-    ht2.insert(98);
-
-    ht2.remove(98);
-    if (ht2.search(577))
-        std::cout << "577 is in ht2" << std::endl;
+    ht2.remove(987777);
+    if(ht2.search(987777))
+        std::cout<<"after removing 987777, 987777 is still in ht2"<<std::endl;
     else
-        std::cout << "577 is NOT in ht2" << std::endl;
+        std::cout<<"after removing 987777, 987777 is NOT in ht2"<<std::endl;    
+    std::cout<<"the size of ht2 now is " << ht2.size() << std::endl; 
+    std::cout << std::endl;
 
-    if (ht2.search(98))
-        std::cout << "98 is in ht2" << std::endl;
+    ht2.remove(987777);
+    if(ht2.search(987777))
+        std::cout<<"after second removing 987777, 987777 is still in ht2"<<std::endl;
     else
-        std::cout << "98 is NOT in ht2" << std::endl; 
-
-    std::cout << "ht2 now contains " << ht2.size() << " elements" << std::endl;  
+        std::cout<<"after second removing 987777, 987777 is NOT in ht2"<<std::endl;    
+    std::cout<<"the size of ht2 now is " << ht2.size() << std::endl;    
 
 
     return 0;
 }
 
-HashTable::LinkedList::LinkedList (): nodeCount{0}, head{nullptr} {}
+HashTable::LinkedList::LinkedList(): nodeCount{0}, head{nullptr} {}
 
-HashTable::LinkedList::Node* HashTable::LinkedList::buildNode (int val)
+
+HashTable::LinkedList::~LinkedList()
+{
+    cleanList();
+}
+
+
+HashTable::LinkedList::Node* HashTable::LinkedList::buildNode(int val)
 {
     Node* ptr = new Node;
     ptr->data = val;
@@ -142,7 +163,12 @@ HashTable::LinkedList::Node* HashTable::LinkedList::buildNode (int val)
     return ptr;
 }
 
-void HashTable::LinkedList::sortedInsert (int val)
+size_t HashTable::LinkedList::getNodeCount() const
+{
+    return nodeCount;
+}
+
+void HashTable::LinkedList::insertToList(const int val)
 {
     if (head == nullptr)
     {
@@ -153,99 +179,116 @@ void HashTable::LinkedList::sortedInsert (int val)
     Node* temp = head;
     while(temp)
     {
-        if(temp->next == nullptr || temp->next->data >= val)
+        if(temp->data == val)
+            return;
+
+        if(temp->next == nullptr)
         {
-            Node* temp2 = buildNode(val);
-            temp2->next = temp->next;
-            temp->next = temp2;
-            break;
+            temp->next = buildNode(val);
+            return;
         }
-        temp = temp->next;
     }
+    temp = temp->next;
 }
 
-int HashTable::LinkedList::getNodeCount () const
+void HashTable::LinkedList::removeFromList(const int val)
 {
-    return nodeCount;
-}
+    if(nodeCount == 0)
+        return;
 
-bool HashTable::LinkedList::search (int val) const
-{
-    if(nodeCount == 0) return false;
-    
-    if (nodeCount == 1 && head->data == val) return true;
-
-    Node* temp = head;
-
-    while (temp && temp->next->data <= val)
-    {
-        if(temp->data == val) return true;
-        temp = temp->next;
-    }
-
-    return false;
-}
-
-void HashTable::LinkedList::remove (int val)
-{
-    if (nodeCount == 0) return; 
     
     Node* temp = head;
     if(temp->data == val)
-    {       
-        head = temp->next;
-        delete temp;
-        --nodeCount;
-        return;
+    {
+        if(temp->next)
+        {
+            head = temp->next;
+            delete temp;
+            nodeCount--;
+            return;
+        }
+        else
+        {
+            head = nullptr;
+            delete temp;
+            nodeCount--;
+            return;
+        }
+        
     }
-
     Node* temp2 = temp->next;
-
-    while (temp2 && temp2->data <= val)
+    while(temp2)
     {
         if(temp2->data == val)
         {
-            temp->next = temp2->next;
-            delete temp2;
-            --nodeCount;
-            return;
+            if(temp2->next == nullptr)
+            {
+                temp->next = nullptr;
+                delete temp2;                
+                nodeCount--;
+                return;
+            }
+            else
+            {
+                temp->next = temp2->next;
+                delete temp2;                
+                nodeCount--;
+                return;
+            }
+            temp = temp->next;
+            temp2 = temp2->next;
         }
-        temp = temp->next;
-        temp2 = temp2->next;
-    } 
+    }
 }
 
-void HashTable::LinkedList::clean (Node* ptr)
+bool HashTable::LinkedList::searchInList(const int val) const
 {
-    if (nodeCount == 0) return;
+    if(nodeCount == 0)
+        return false;
 
-    Node* temp = ptr;
+    Node* temp = head;
 
     while (temp)
-    {   
-        Node* current = temp;
-        delete current;
+    {
+        if(temp->data == val)
+            return true;
+        
         temp = temp->next;
     }
-
+    
+    return false;
 }
 
-HashTable::HashTable (): totalCountOfEntries{0} {}
+void HashTable::LinkedList::cleanList () noexcept
+{
+    if(head == nullptr)
+        return;
 
-HashTable::HashTable (int val): HashTable()
+    Node* temp = head;
+    Node* current = nullptr;
+    while(temp)
+    {
+        current = temp;
+        temp = temp->next;
+        delete current;        
+    }
+    head = nullptr;
+}
+
+HashTable::HashTable(): arrayOfLinkedLists{new LinkedList [20]} {}
+
+HashTable::HashTable(const int val): HashTable()
 {
     insert(val);
 }
 
-HashTable::~HashTable ()
-{
-    for (int i = 0; i < 20; i++)
-        arrayOfLinkedLists[i].clean(arrayOfLinkedLists[i].head);
-
+HashTable::~HashTable()
+{   
     delete [] arrayOfLinkedLists;
+    arrayOfLinkedLists = nullptr;    
 }
 
-int HashTable::hashFunction (int val) const
+int HashTable::hashFunction(const int val) const
 {
     int y = val % 10;
 
@@ -254,33 +297,33 @@ int HashTable::hashFunction (int val) const
     return y; 
 }
 
-void HashTable::insert (int val)
+void HashTable::insert(const int val)
 {
     int y = hashFunction(val);
 
-    arrayOfLinkedLists[y + 10].sortedInsert(val);
-
-    totalCountOfEntries++;
+    arrayOfLinkedLists[y + 10].insertToList(val);
 }
 
-int HashTable::size () const
-{
-    return totalCountOfEntries;
-}
-
-bool HashTable::search (int val) const
+void HashTable::remove(const int val)
 {
     int y = hashFunction(val);
 
-    return arrayOfLinkedLists[y + 10].search(val);
+    arrayOfLinkedLists[y + 10].removeFromList(val);
 }
 
-void HashTable::remove (int val)
+bool HashTable::search(const int val) const
 {
     int y = hashFunction(val);
 
-    arrayOfLinkedLists[y + 10].remove(val);
+    return arrayOfLinkedLists[y + 10].searchInList(val);
+}
 
-    totalCountOfEntries--;
+size_t HashTable::size() const
+{
+    size_t sum = 0;
+    for(size_t i = 0; i<20; i++)
+        sum += arrayOfLinkedLists[i].getNodeCount();
+
+    return sum;
 }
 
